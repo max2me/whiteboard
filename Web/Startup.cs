540a670a0d8d3fc4f06a27cc345079963139b699
Maxim Soloviev <max@max2.me>
2016-11-28 11:08:21 -0800

    123

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -8,17 +8,6 @@ namespace Napkin
 {
     public class Startup
     {
-		public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-
         public void ConfigureServices(IServiceCollection services)
         {
 	        services
