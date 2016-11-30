4b3088bd3cc8af49750873f431c153d228d80624
Maxim Soloviev <max@max2.me>
2016-11-29 19:04:49 -0800

    Added multiline support for text.

--- Napkin/wwwroot/scripts/app.js
+++ Napkin/wwwroot/scripts/app.js
@@ -840,10 +840,14 @@ var Drawers;
             _super.apply(this, arguments);
         }
         Text.prototype.text = function (item, last, zoom) {
+            var lines = (item.text + (last ? '_' : '')).split('\n');
             var size = 30 * item.fontSizeK * zoom;
             this.ctx.font = size + "px 	'Permanent Marker'";
             this.ctx.fillStyle = 'black';
-            this.ctx.fillText(item.text + (last ? '_' : ''), item.raw[0].x, item.raw[0].y);
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                this.ctx.fillText(line, item.raw[0].x, item.raw[0].y + (size + 3) * i);
+            }
         };
         return Text;
     }(Drawers.Base));
--- Napkin/wwwroot/typescript/drawers/text.ts
+++ Napkin/wwwroot/typescript/drawers/text.ts
@@ -1,10 +1,15 @@
 namespace Drawers {
 	export class Text extends Base {
 		text(item: Item, last: boolean, zoom: number) {
+			var lines = (item.text + (last ? '_' : '')).split('\n');
 			var size = 30 * item.fontSizeK * zoom;
 			this.ctx.font = size + "px 	'Permanent Marker'";
 			this.ctx.fillStyle = 'black';
-			this.ctx.fillText(item.text + (last ? '_' : ''), item.raw[0].x, item.raw[0].y);		
+
+			for (var i = 0; i < lines.length; i++) {
+				var line = lines[i];
+				this.ctx.fillText(line, item.raw[0].x, item.raw[0].y + (size+3) * i);
+			}
 		}
 	}
 }
